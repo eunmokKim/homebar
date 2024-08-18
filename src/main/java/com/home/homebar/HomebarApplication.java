74409f83b912a6cbe2faf5f42cc25e6febd09adf
silvermok <rladmsahr92@naver.com>
2024-08-18 15:21:12 +0900

    init

---
+++ src/main/java/com/home/homebar/HomebarApplication.java
@@ -0,0 +1,13 @@
+package com.home.homebar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomebarApplication.class, args);
+    }
+
+}
